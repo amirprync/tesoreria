00d4656dcad236b65e2833b2a33f142d66d49eb1
amirprync <60889238+amirprync@users.noreply.github.com>
2023-05-08 19:43:41 -0300

    Update App.py

--- App.py
+++ App.py
@@ -13,7 +13,7 @@ def process_file(file):
         nombre = str(fila['NOMBRE'])
         importe = str(fila['IMPORTE'])
 
-        registro = f'DR2;900100000102553;{cbu};;{cuit};{nombre};{importe};VAR;N;N;TRANSF COHEN;\n'
+        registro = f'DL2;900100000102553;{cbu};;{cuit};{nombre};{importe};VAR;N;N;TRANSF COHEN;\n'
         output.write(registro)
 
     output.seek(0)
